5723cf5738cffe4d8fb753d93013102dea987602
Seckene Sako <seckene_sako@hotmail.com>
2025-01-28 15:18:57 +0100

    nouvjs

--- exenple1.js
+++ exenple1.js
@@ -2,15 +2,15 @@
 const liste=document.querySelector(".liste");
 window.addEventListener("scroll",()=>{
     console.log(window.scrollY);
-    if (window.scrollY>20) {
+    if (window.scrollY>10) {
         liste.style.visibility="hidden";
-        liste.style.background="black"
+      
 
     }
 
     else{   
         
-        window.scrollY<160
+        window.scrollY>160
         liste.style.visibility="visible";
          liste.style.top="1px";
          
